47771f41534983e34890ab22a23c843a5b82761d
Jordan Ramos <jordan.ramos@timeinc.com>
2016-07-08 16:10:04 -0700

    Progress

--- app/containers/Dashboard.jsx
+++ app/containers/Dashboard.jsx
@@ -1,14 +1,17 @@
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
 import { composeWithTracker } from 'react-komposer';
+
+import { getArticles } from '../../lib/methods';
 
 import Publications from '../../lib/collections/Publications';
 import Dashboard from '../components/Dashboard.jsx';
 
 const composer = ( props, onData ) => {
-  if ( Meteor.subscribe( 'publications' ).ready( ) ) {
-    const data = Publications.find().fetch();
+  const data = Meteor.call( 'getArticles', 'time' );
+  // if ( Meteor.subscribe( 'publications' ).ready( ) ) {
+    // const data = Publications.find().fetch();
     onData( null, { data } );
-  };
+  // };
 };
 
 export default composeWithTracker( composer )( Dashboard );
